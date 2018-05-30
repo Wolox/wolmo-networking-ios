311147ff3fac147974d818e07d253f4511b3bc81
Nahuel Gladstein <nahuel.gladstein@wolox.com.ar>
2018-05-30 16:14:24 -0300

    Add correct markdown for documentation of NetworkingConfiguration

--- Networking/Model/NetworkingConfiguration.swift
+++ Networking/Model/NetworkingConfiguration.swift
@@ -23,40 +23,36 @@ private enum CommunicationProtocol: String {
     It's the only place where these necessary parameters are configured.
  */
 public struct NetworkingConfiguration {
-    /**
-     - Parameters
-     - useSecureConnection: a boolean representing whether the requests
-     will be made using a secure protocol. By default it's enabled.
-     Take into account in case this is disabled, the appropriate
-     exclusions must be added to plist file.
-     - domainURL: the base url the requests will be performed against.
-     - port: the port the requests will be performed against. By default
-     there is no specific port.
-     - subdomainURL: the subdomain url to be appended to domainURL to build
-     the final url (it can be used to specify API versioning). By default it's empty.
-     This url, as a path of the domainURL must start with "/".
-     - usePinningCertificate: a boolean representing if SSL Pinning will be
-     enabled for the performed requests. By default it's disabled. Take into
-     account in case this is enabled, the proper certificate must be included
-     into the application bundle resources.
-     - timeout: the timeout of the requests in seconds. It defaults to 75 seconds.
-     - secondsBetweenPolls: for polling requests, seconds between one polling and the next. It defaults to 1 second.
-     - maximumPollingRetries: Maximum retries until a polling request gives timeout. If it's not set then it will use timeout/secondsBetweenPolls
-     */
-    
+    /// A boolean representing whether the requests will be made using a secure protocol. By default it's enabled.
+    /// Take into account in case this is disabled, the appropriate exclusions must be added to plist file.
     public var useSecureConnection: Bool = true
+    
+    /// The base url the requests will be performed against.
     public var domainURL: String = ""
+    
+    /// The port the requests will be performed against. By default there is no specific port.
     public var port: Int? = .none
+    
+    /// The subdomain url to be appended to domainURL to build the final url (it can be used to specify API versioning). By default it's empty.
+    /// This url, as a path of the domainURL must start with "/".
     public var subdomainURL: String? = .none
+    
+    /// A boolean representing if SSL Pinning will be enabled for the performed requests. By default it's disabled.
+    /// Take into account in case this is enabled, the proper certificate must be included into the application bundle resources.
     public var usePinningCertificate: Bool = false
+    
+    /// The timeout of the requests in seconds. It defaults to 75 seconds.
     public var timeout: Double = 75.0
+    
+    /// For polling requests, seconds between one polling and the next. It defaults to 1 second.
     public var secondsBetweenPolls: Double = 1.0
+    
+    /// Maximum retries until a polling request gives timeout. If it's not set then it will use timeout/secondsBetweenPolls
     public var maximumPollingRetries: Int? = .none
     
     /**
          Initializes the networking configuration with default values.
      */
-    
     public init() {}
 
 }
